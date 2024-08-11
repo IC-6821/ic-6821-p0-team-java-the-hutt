24669efe0dfbc8873cd671116b5a928a631e7846
kyaki <emanuelrs101@gmail.com>
2024-08-11 01:10:51 -0600

    Completed cell class and board class, board contains most of the logic needed to almost play the game, will be implementing Game class soon

--- app/src/main/java/com/modules/logic/Board.java
+++ app/src/main/java/com/modules/logic/Board.java
@@ -1,3 +1,114 @@
 package com.modules.logic;
 
 
+
+class Board{
+
+    private Cell[][] board;
+
+
+
+    /**
+      * Creates empty board to start new game.
+      * Cell constructor is in charge of adding empty spaces on each cell.
+      */
+    Board(){
+        board = new Cell[3][3];
+        
+        /**
+         * Creating the array only allocates memory but doesn't initialize objects, so it has to iterate through every cell of initialize it.
+         */
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                board[i][j] = new Cell();
+            }
+        }
+
+
+    }
+
+    public Cell getCell(int i, int j){
+        return board[i][j];
+    }
+
+
+
+
+    /**
+     * This function will determine if the player or the computer has won the game
+     */
+    
+
+    /**
+     * These function will determine if the player won by filling a column.
+     * It will do so by checking if each element of one of the columns is the same as symbol, which will receive 'X' or 'O'.
+     */
+
+    private boolean leftColumnWin(char symbol){
+        return board[0][0].getSymbol() == symbol && board[1][0].getSymbol() == symbol && board[2][0].getSymbol() == symbol;
+    }
+
+    private boolean midColumnWin(char symbol){
+        return board[0][0].getSymbol() == symbol && board[1][0].getSymbol() == symbol && board[2][0].getSymbol() == symbol;
+    }
+
+    private boolean rightColumnWin(char symbol){
+        return board[0][2].getSymbol() == symbol && board[1][2].getSymbol() == symbol && board[2][2].getSymbol() == symbol;
+    }
+
+
+
+
+    /**
+     * These function will determine if the player won by filling a row with the same symbol.
+     */
+    private boolean topRowWin(char symbol){
+        return board[0][0].getSymbol() == symbol && board[0][1].getSymbol() == symbol && board[0][2].getSymbol() == symbol;
+    }
+    
+    private boolean midRowWin(char symbol){
+        return board[1][0].getSymbol() == symbol && board[1][1].getSymbol() == symbol && board[1][2].getSymbol() == symbol;
+    }
+
+    private boolean bottomRowWin(char symbol){
+        return board[2][0].getSymbol() == symbol && board[2][1].getSymbol() == symbol && board[2][2].getSymbol() == symbol;
+    }
+
+
+
+    /**
+     * These functions will determine if the player won by placing the same symbol diagonally
+     */
+    
+    private boolean mainDiagonalWin(char symbol){
+        return board[0][0].getSymbol() == symbol && board[1][1].getSymbol() == symbol && board[2][2].getSymbol() == symbol;
+    }
+
+    private boolean secondaryDiagonalWin(char symbol){
+        return board[0][2].getSymbol() == symbol && board[1][1].getSymbol() == symbol && board[2][0].getSymbol() == symbol;
+    }
+
+
+
+
+    
+    /**
+     * this function will verify if the player or bot won in any possible way
+     */
+
+    public boolean verifyWin(char symbol){
+        return leftColumnWin(symbol) || 
+                midColumnWin(symbol) ||
+                rightColumnWin(symbol) || 
+                topRowWin(symbol) ||
+                midRowWin(symbol) ||
+                bottomRowWin(symbol) ||
+                mainDiagonalWin(symbol) ||
+                secondaryDiagonalWin(symbol);
+
+    }
+
+
+}
+
+
