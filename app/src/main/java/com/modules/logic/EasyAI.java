baed546b9e19c096a092f20e4d9051157b369960
AleQuesada2012 <ale11199@estudiantec.cr>
2024-08-13 19:58:22 -0600

    change `do-while` control structure
    Co-authored-by: josh_lis <josh.solisf@gmail.com>
    
    removed the old logic and changed it for a more readable while loop.

--- app/src/main/java/com/modules/logic/EasyAI.java
+++ app/src/main/java/com/modules/logic/EasyAI.java
@@ -10,12 +10,13 @@ public class EasyAI {
 
     public int[] getRandomMove(Board board) {
         int[] move = new int[2];
-        int row, column;
+        int row = random.nextInt(3);
+        int column = random.nextInt(3);
 
-        do {
+        while (!board.canPlace(row, column)){
             row = random.nextInt(3);
             column = random.nextInt(3);
-        } while (!board.canPlace(row, column));
+        }
 
         move[0] = row;
         move[1] = column;
