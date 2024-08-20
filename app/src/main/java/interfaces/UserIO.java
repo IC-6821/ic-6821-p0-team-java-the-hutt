a9ed5e80704e62f95d17a6a68251c2041e1177c0
Alejandro <ale11199@estudiantec.cr>
2024-08-19 21:31:48 -0600

    remove redundant visibility modifier

--- app/src/main/java/interfaces/UserIO.java
+++ app/src/main/java/interfaces/UserIO.java
@@ -6,6 +6,6 @@ import org.classes.GameLevel;
 public interface UserIO {
     void showToPlayer(String message); //changed from ShowBoard to showToPlayer
     void processArguments(String[] args);
-    public BoardPosition interpretPlayerMove(); // no se pasa el string aquí porque se pide dentro.
+    BoardPosition interpretPlayerMove(); // no se pasa el string aquí porque se pide dentro.
     GameLevel getChosenLevel();
 }
